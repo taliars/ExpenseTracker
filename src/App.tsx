9a4b88a2d1a7592403fba427f6681c030a4367f3
Stanislav Taliar <agnosiac@gmail.com>
2022-04-01 21:30:26 +0300

    Add fetching data from url

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-import { expenseData } from "./expensesDatas";
 import ExpenseData from "./model/ExpenseData";
 
-const App = () => {
-  const [expenses, setExpenses] = useState(expenseData);
+const baseURL =
+  "https://expensetracker-e36c2-default-rtdb.firebaseio.com/expenses.json";
 
-  const addExpenseHanlder = (expense: ExpenseData) =>
-    setExpenses([ expense, ...expenses ]);
+const App = () => {
+  const [expenses, setExpenses] = useState<ExpenseData[]>([]);
+
+  const getExpensesHandler = async () => {
+    try {
+      const res = await axios.get<ExpenseData[]>(baseURL);
+      if (res.data) {
+        const expenses = Object.values(res.data).map((x) => ({
+          ...x,
+          date: new Date(x.date),
+        }));
+        setExpenses(expenses);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    }
+  };
+
+  const addExpenseHanlder = async (newExpense: ExpenseData) => {
+    setExpenses([newExpense, ...expenses]);
+    await axios.post(baseURL, newExpense);
+  };
+
+  useEffect(() => {
+    getExpensesHandler();
+  }, []);
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHanlder}/>
-      <Expenses expenses={expenses}/>
+      <NewExpense onAddExpense={addExpenseHanlder} />
+      <Expenses expenses={expenses} />
     </div>
   );
 };
